f7d0525aed60b465b811d646b01f32a84e7008c2
Astrid-Macana <lorenamakna21@gmail.com>
2022-08-17 12:02:31 -0500

    trabajando en funcion ordenar

--- src/data.js
+++ src/data.js
@@ -3,73 +3,66 @@
 
 import athletes from "./data/athletes/athletes.js";
  
-export const dataFilter = (country, gender, medal) => {
-  //const allData = Object.values(athletes.athletes);
-  //let filterSport = allData.filter(athletes => athletes.sport == sports);
-  //let filterSport = athletes.athletes.filter(athletes => athletes.sport == sports);//con este tengo  que ser mas exacta
-  // let filterSport = athletes.athletes.filter(
-  //   (athletes) => athletes.sport.toLowerCase().includes(sports.toLowerCase()) //con este toma concidencias
-  // );
-  let filterCountry = athletes.athletes;
-  let filterGender = filterCountry;
-  let filterMedal = filterGender;
-  if (country != "all") {
-    filterCountry = filterCountry.filter(
-      (athletes) => athletes.team.toLowerCase().includes(country.toLowerCase()) //con este toma concidencias
-    );
-    filterGender = filterCountry;
-    filterMedal = filterGender;
-  }
+export const dataFilter= (country,gender,medal)=>{
+ let filterCountry = athletes.athletes;
+ let filterGender =filterCountry;
+ let filterMedal=filterGender;
+
+ if (country!="all"){
+    filterCountry=filterCountry.filter((athletes)=>athletes.team.includes(country));
+   filterGender = filterCountry;
+   filterMedal = filterGender;
+ }
 
   if (gender != "all") {
-    filterGender = filterGender.filter(
-      (athletes) => athletes.gender.toLowerCase().includes(gender.toLowerCase()) //con este toma concidencias
+    filterGender = filterGender.filter((athletes) =>
+      athletes.gender.includes(gender)
     );
     filterMedal = filterGender;
   }
-
   if (medal != "all") {
-    filterMedal = filterGender.filter(
-      (athletes) => athletes.medal.toLowerCase().includes(medal.toLowerCase()) //con este toma concidencias
+    filterMedal = filterMedal.filter((athletes) =>
+      athletes.medal.includes(medal)
     );
   }
+ 
+ return filterMedal;
+}
+// listas desplegables
 
-  return filterMedal;
-};
-
-let countriesArray = []; //vector
-
-for (let country of athletes.athletes) {
+export const countryData=()=>{
+let countriesArray = [];
+for (const country of athletes.athletes) {
   countriesArray.push(country.team);
 }
 
-export const countryData = countriesArray
-  .filter((country, pos) => {
-    //array de strings
-    return countriesArray.indexOf(country) == pos; //me retorna el primer indice en el se encuntra un elemento
-  })
-  .sort(); // organiza por orden alfabetico
+return countriesArray.filter((country,pos)=>{
+    return countriesArray.indexOf(country)==pos
+}).sort();//permite organizar de menor a mayor un array
 
-let gendersArray = [];
+};
 
-for (let gender of athletes.athletes) {
-  gendersArray.push(gender.gender);
-}
+export const genderData = () => {
+  let gendersArray = [];
+  for (const gender of athletes.athletes) {
+    gendersArray.push(gender.gender);
+  }
 
-export const genderData = gendersArray
-  .filter((gender, pos) => {
-    return gendersArray.indexOf(gender) == pos;
-  })
-  .sort();
+  return gendersArray.filter((gender, pos) => {
+      return gendersArray.indexOf(gender) == pos;
+    })
+    .sort(); //permite organizar de menor a mayor un array
+};
+export const medalData = () => {
+  let medalsArray = [];
+  for (const medal of athletes.athletes) {
+    medalsArray.push(medal.medal);
+  }
 
-let medalArray = [];
+  return medalsArray.filter((medal, pos) => {
+     return medalsArray.indexOf(medal) == pos;
+    })
+    .sort(); //permite organizar de menor a mayor un array
+};
 
-for (let medal of athletes.athletes) {
-  medalArray.push(medal.medal);
-}
 
-export const medalData = medalArray
-  .filter((medal, pos) => {
-    return medalArray.indexOf(medal) == pos;
-  })
-  .sort();
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { countryData, genderData, medalData, dataFilter } from "./data.js";
+//import { countryData, genderData, medalData, dataFilter } from "./data.js";
 //import data from './data/athletes/athletes.js';
 
 /**Alertas de errores con ventana emergente */
@@ -7,58 +7,66 @@ import { countryData, genderData, medalData, dataFilter } from "./data.js";
 //function mialerta() {
 ///swal("Oops!", "Something went wrong on the page!", "error");
 
-console.log(dataFilter("Colombia", "all", "all"));
 
-//CREAR SELECT DE FILTRO DE PAISES
-let optionsCountry = '<option selected value="all">Todos</option>';
-for (const contry of countryData) {
-  optionsCountry += '<option value="' + contry + '">' + contry + "</option>";
+import { dataFilter,countryData,genderData,medalData } from "./data.js";
+
+console.log(countryData());
+console.log(genderData());
+console.log(medalData());
+
+//selc country
+let optionsCountry="<option value=\"all\">Todos</option>";
+for (const country of countryData()) {
+  optionsCountry += '<option value="'+country+'">'+country+'</option>';
+
 }
-
 document.getElementById("selectCountry").innerHTML = optionsCountry;
 
-//CREAR SELECT DE FILTRO DE GENEROS
-let optionsGender = '<option selected value="all">Todos</option>';
-for (const gender of genderData) {
-  optionsGender += "<option value=" + gender + ">" + gender + "</option>";
-}
+//selc gender
 
+let optionsGender = '<option value="all">Todos</option>';
+for (const gender of genderData()) {
+  optionsGender += '<option value="' + gender + '">' + gender+ "</option>";
+}
 document.getElementById("selectGender").innerHTML = optionsGender;
 
-//CREAR SELECT DE FILTRO DE MEDALLAS
-let optionsMedal = '<option selected value="all">Todos</option>';
-for (const medal of medalData) {
-  optionsMedal += "<option value=" + medal + ">" + medal + "</option>";
-}
+//selec medal
 
+let optionsMedal = '<option value="all">Todos</option>';
+for (const medal of medalData()) {
+  optionsMedal += '<option value="' + medal+ '">' + medal + "</option>";
+}
 document.getElementById("selectMedal").innerHTML = optionsMedal;
 
-//BOTON DE FILTRO
 document.getElementById("filters").addEventListener("click", filterByCGM);
 
-function filterByCGM() {
+//ordenar az
+// let arrayOrdenar=dataFilter();
+// Array.sort(arrayOrdenar);
+// for ( let i = 0; i < arrayOrdenar.length;i++)
+//  console.log(arrayOrdenar);
+
+
+   
+//   let optionsOrdenar = '<option value="all">Todos</option>';
+//  for (const ordenador of arrayOrdenar) {
+//    optionsOrdenar += '<option value="' + ordenador+ '"> '+ "</option>";
+//  }
+  
+
+// document.getElementById("selecTa-z").innerHTML=optionsOrdenar;
+//  document.getElementById("odenar").addEventListener("click",arrayOrdenar);
+
+//tabla
+function filterByCGM(){
   let country = document.getElementById("selectCountry").value;
   let gender = document.getElementById("selectGender").value;
   let medal = document.getElementById("selectMedal").value;
-
-  console.log(dataFilter(country, gender, medal));
-  let data = dataFilter(country, gender, medal);
-
-  //FROMA DE CREAR TABLAS
-  let table =
-    "<tr>" +
-    "<th>Nombre</th>" +
-    "<th>Genero</th>" +
-    "<th>Deporte</th>" +
-    "<th>País</th>" +
-    "<th>Especialidad</th>" +
-    "<th>Medalla</th>" +
-    "</tr>";
-
-  for (const reg of data) {
-    //reg de registro
-    let row = '<tr class="search"><td>';
-    row += reg.name; //row = row + reg.name;
+  let arrayData = dataFilter(country, gender, medal);
+  let table = "";
+  for (const reg of arrayData) {
+    let row = "<tr class=\"search\"><td>";
+    row += reg.name;
     row += "</td>";
 
     row += "<td>";
@@ -79,27 +87,49 @@ function filterByCGM() {
 
     row += "<td>";
     row += reg.medal;
-    row += "</td></tr>";
+    row += "</td> </tr>";
 
     table += row;
+   
+    
   }
+      document.getElementById("tbl").innerHTML = table;
+     document.getElementById("search").value = "";
 
-  document.getElementById("tbl").innerHTML = table;
-  document.getElementById("search").value = "";
-  console.log(data);
+
+
+  //FILTRO DE BUSQUEDA
+  document.addEventListener("keyup", (evt) => {//keyup, oprimir y soltar una tecla
+    if (evt.target.matches("#search")) {// target referencia al objeto al cual se lanzo el evento
+      document.querySelectorAll(".search").forEach((row) => {
+        row.textContent.toLowerCase().includes(evt.target.value.toLowerCase()) //operador de sigo
+          ? row.classList.remove("filter") //no oculta
+          : row.classList.add("filter"); //oculta
+      });
+    }
+  });
+
+  console.log(table);
 }
+window.onload = filterByCGM();
 
-//FILTRO DE BUSQUEDA
-document.addEventListener("keyup", (evt) => {
-  //keyup, oprimir y soltar una tecla
-  if (evt.target.matches("#search")) {
-    // target referencia al objeto al cual se lanzo el evento
-    document.querySelectorAll(".search").forEach((row) => {
-      row.textContent.toLowerCase().includes(evt.target.value.toLowerCase()) //operador de sigo
-        ? row.classList.remove("filter") //no oculta
-        : row.classList.add("filter"); //oculta
-    });
-  }
-});
 
-window.onload = filterByCGM; //carga la tabla al cargar la pagína por priemra vez
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+  
+
+
+
