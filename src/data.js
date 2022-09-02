120afd7de2dfe0a2617d77277ab30d9d49173215
Astrid Macana <lorenamakna21@gmail.com>
2022-09-01 19:26:09 -0500

    solucioado los conflictos

--- src/data.js
+++ src/data.js
@@ -1,74 +1,82 @@
+export const dataFilter = (athletes, country, gender, medal) => {
+        let filterCountry = athletes.athletes;
+        let filterGender = filterCountry;
+        let filterMedal = filterGender;
+        if (country != "all") {
+            filterCountry = filterCountry.filter((athletes) =>
+                athletes.team.includes(country)
+            );
+            filterGender = filterCountry;
+            filterMedal = filterGender;
+        }
+        if (gender != "all") {
+            filterGender = filterGender.filter((athletes) =>
+                athletes.gender.includes(gender)
+            );
+            filterMedal = filterGender;
+        }
+        if (medal != "all") {
+            filterMedal = filterMedal.filter((athletes) =>
+                athletes.medal.includes(medal)
+            );
+        }
 
+        return filterMedal;
 
-// import athletes from "./data/athletes/athletes.js";
- 
-export const dataFilter = (athletes,country, gender, medal) => {
-  let filterCountry = athletes.athletes;
-  let filterGender = filterCountry;
-  let filterMedal = filterGender;
-
-  if (country != "all") {
-    filterCountry = filterCountry.filter((athletes) =>
-      athletes.team.includes(country)
-    );
-    filterGender = filterCountry;
-    filterMedal = filterGender;
-  }
-
-  if (gender != "all") {
-    filterGender = filterGender.filter((athletes) =>
-      athletes.gender.includes(gender)
-    );
-    filterMedal = filterGender;
-  }
-  if (medal != "all") {
-    filterMedal = filterMedal.filter((athletes) =>
-      athletes.medal.includes(medal)
-    );
-  }
-  
-  return filterMedal;
-};
-// listas desplegables
-
-
+    }
+    // funcion para hcer una lista de paises
 
 export const countryData = (athletes) => {
-  let countriesArray = [];
-  for (const country of athletes.athletes) {
-    countriesArray.push(country.team);
-  }
-
-  return countriesArray
-    .filter((country, pos) => {
-      return countriesArray.indexOf(country) == pos;
-    })
-    .sort(); //permite organizar de menor a mayor un array
+    let countriesArray = [];
+    for (const country of athletes.athletes) {
+        countriesArray.push(country.team);
+    }
+    return countriesArray
+        .filter((country, pos) => {
+            return countriesArray.indexOf(country) == pos;
+        })
+        .sort(); //permite organizar de menor a mayor un array
 };
 
 export const genderData = (athletes) => {
-  let gendersArray = [];
-  for (const gender of athletes.athletes) {
-    gendersArray.push(gender.gender);
-  }
+    let gendersArray = [];
+    for (const gender of athletes.athletes) {
+        gendersArray.push(gender.gender);
+    }
+    // console.log(gendersArray)
 
-  return gendersArray
-    .filter((gender, pos) => {
-      return gendersArray.indexOf(gender) == pos;
-    })
-    .sort(); //permite organizar de menor a mayor un array
+    return gendersArray
+        .filter((gender, pos) => {
+            return gendersArray.indexOf(gender) == pos;
+        })
+        .sort(); //permite organizar de menor a mayor un array
 };
+
 export const medalData = (athletes) => {
-  let medalsArray = [];
-  for (const medal of athletes.athletes) { 
-    medalsArray.push(medal.medal);
-  }
-
-  return medalsArray
-    .filter((medal, pos) => {
-      return medalsArray.indexOf(medal) == pos;
-    })
-    .sort(); //permite organizar de menor a mayor un array
+    let medalsArray = [];
+    for (const medal of athletes.athletes) {
+        medalsArray.push(medal.medal);
+    }
+    return medalsArray
+        .filter((medal, pos) => {
+            return medalsArray.indexOf(medal) == pos;
+        })
+        .sort(); //permite organizar de menor a mayor un array
 };
 
 
+// funcion calcular
+export const percentage = (dataaa) => {
+
+    let auxWomen = 0;
+    dataaa.map((index) => {
+        if (index.gender === "F") {
+            auxWomen++;
+        }
+    })
+
+    let porWomen = (auxWomen * 100) / dataaa.length;
+    let result = Number(porWomen.toFixed(2));
+
+    return result;
+};
--- src/main.js
+++ src/main.js
@@ -1,142 +1,162 @@
-
 import athletes from "./data/athletes/athletes.js";
+import { percentage, countryData, genderData, medalData, dataFilter, } from "./data.js";
 
-//function mialerta() {
-///swal("Oops!", "Something went wrong on the page!", "error");
-
-
-import { dataFilter,countryData,genderData,medalData } from "./data.js";
-
- console.log(countryData(athletes));
-// console.log(genderData(athletes));
-// console.log(medalData(athletes));
-// console.log(dataFilter(athletes));
-
-//selc country
-let optionsCountry="<option value=\"all\">Todos</option>";
+//CREAR SELECT DE FILTRO DE PAISES
+let optionsCountry = "<option value=\"all\">Todos</option>";
 for (const country of countryData(athletes)) {
-  optionsCountry += '<option value="' + country + '">' + country + "</option>";
+    optionsCountry += '<option value="' + country + '">' + country + "</option>";
 }
 document.getElementById("selectCountry").innerHTML = optionsCountry;
 
-//selc gender
-
+//CREAR SELECT DE FILTRO DE GENEROS
 let optionsGender = '<option value="all">Todos</option>';
 for (const gender of genderData(athletes)) {
-  optionsGender += '<option value="' + gender + '">' + gender + "</option>";
+    optionsGender += '<option value="' + gender + '">' + gender + "</option>";
 }
 document.getElementById("selectGender").innerHTML = optionsGender;
 
-//selec medal
-
+//CREAR SELECT DE FILTRO DE MEDALLAS
 let optionsMedal = '<option value="all">Todos</option>';
 for (const medal of medalData(athletes)) {
-  optionsMedal += '<option value="' + medal + '">' + medal + "</option>";
+    optionsMedal += '<option value="' + medal + '">' + medal + "</option>";
 }
 document.getElementById("selectMedal").innerHTML = optionsMedal;
 
+//ORDENAR DATA
 document.getElementById("btn").addEventListener("click", orderByName);
 
 function orderByName() {
-  let btnValue = document.getElementById("btn").value;
-  let order = "";/*inicializo la variable */
-  if (btnValue == "1") {
-    order = "1";
-    document.getElementById("btn").value = "2";
-  } else {
-    order = "2";
-    document.getElementById("btn").value = "1";
-  }
-  filterByCGM(order);
+    let btnValue = document.getElementById("btn").value;
+    let order = ""; /*inicializo la variable */
+    if (btnValue == "1") {
+        order = "1";
+        document.getElementById("btn").value = "2";
+    } else {
+        order = "2";
+        document.getElementById("btn").value = "1";
+    }
+    filterByCGM(order);
 }
 
-
-
-//tabla
+//BOTON DE FILTRO
 document.getElementById("filters").addEventListener("click", filterByCGM);
 
 function filterByCGM(order) {
-  let country = document.getElementById("selectCountry").value;
-  let gender = document.getElementById("selectGender").value;
-  let medal = document.getElementById("selectMedal").value;
- 
-  let arrayData = dataFilter(athletes, country, gender, medal);
-   console.log(arrayData);
+    let country = document.getElementById("selectCountry").value;
+    let gender = document.getElementById("selectGender").value;
+    let medal = document.getElementById("selectMedal").value;
 
-  if (order == "1") {
-    arrayData.sort((a, b) => a.name.localeCompare(b.name));
-  }
+    let atletheCount = document.getElementById("atletheCount");
+    let arrayData = dataFilter(athletes, country, gender, medal);
+    let porWomennn = percentage(arrayData);
+    let noencontrado = `
+  <img src="/src/img/no-hay-resultados.png" alt="">
+  `;
+    //FROMA DE CREAR TABLAS
 
-  if (order == "2") {
-    arrayData.sort((a, b) => a.name.localeCompare(b.name)).reverse();
-  }
+    if (order == "1") {
+        arrayData.sort((a, b) => a.name.localeCompare(b.name));
+    }
 
-  let table = "";
+    if (order == "2") {
+        arrayData.sort((a, b) => a.name.localeCompare(b.name)).reverse();
+    }
 
-  for (const reg of arrayData) {
-    let row = '<tr class="search"><td>';
-    row += reg.name;
-    row += "</td>";
+    let table = "";
 
-    row += "<td>";
-    row += reg.gender;
-    row += "</td>";
+    for (const reg of arrayData) {
+        let row = '<tr class="search"><td>';
+        row += reg.name;
+        row += "</td>";
 
-    row += "<td>";
-    row += reg.sport;
-    row += "</td>";
+        row += "<td>";
+        row += reg.gender;
+        row += "</td>";
 
-    row += "<td>";
-    row += reg.team;
-    row += "</td>";
+        row += "<td>";
+        row += reg.sport;
+        row += "</td>";
 
-    row += "<td>";
-    row += reg.event;
-    row += "</td>";
+        row += "<td>";
+        row += reg.team;
+        row += "</td>";
 
-    row += "<td>";
-    row += reg.medal;
-    row += "</td> </tr>";
+        row += "<td>";
+        row += reg.event;
+        row += "</td>";
 
-    table += row;
-  }
-  document.getElementById("tbl").innerHTML = table;
-  document.getElementById("search").value = "";
+        row += "<td>";
+        row += reg.medal;
+        row += "</td> </tr>";
 
-  //FILTRO DE BUSQUEDA
-  document.addEventListener("keyup", (evt) => {
+        table += row;
+    }
+
+    if (arrayData.length > 0) {
+
+        //mostrar pais y pocentaje de mujeres
+        if (country !== "all") {
+            document.getElementById("porcentajeWomen").innerHTML = porWomennn + "%" + "  " + " e Mujeres Participantes";
+            document.getElementById("porcentajeMen").innerHTML = 100 - parseFloat(porWomennn) + "%" + "  " + " De Hombres Participantes";
+            document.getElementById("countryPorcentaje").innerHTML = country;
+
+        }
+
+        if (country === "all") {
+            document.getElementById("porcentajeWomen").innerHTML = porWomennn + "%" + "  " + " De Mujeres Participantes";
+            document.getElementById("porcentajeMen").innerHTML = 100 - parseFloat(porWomennn) + "%" + "  " + " De Hombres Participantes";
+            document.getElementById("countryPorcentaje").innerHTML = "Todos los Paises";
+
+        }
+
+        document.getElementById("tbl").innerHTML = table;
+        //document.getElementById("table").style = " "; // show
+        document.getElementById("search").value = "";
+        atletheCount.innerHTML = arrayData.length; // Pantalla muestra cantidad de atletas
+        document.getElementById("noDatos").style.visibility = "hidden";
+
+    } else {
+        document.getElementById("porcentajeWomen").innerHTML = "0" + "%" + "  " + "De Mujeres Participantes";
+        document.getElementById("porcentajeMen").innerHTML = "0" + "%" + "  " + " De Hombres Participantes";
+        document.getElementById("countryPorcentaje").innerHTML = country;
+        document.getElementById("table").style.display = "none";
+        atletheCount.innerHTML = 0; // Pantalla muestra cantidad de atletas
+        document.getElementById("noDatos").innerHTML = noencontrado;
+        document.getElementById("noDatos").style.visibility = "visible"; // show
+
+    }
+
+}
+//FILTRO DE BUSQUEDA
+document.addEventListener("keyup", (evt) => {
     //keyup, oprimir y soltar una tecla
     if (evt.target.matches("#search")) {
-      // target referencia al objeto al cual se lanzo el evento
-      document.querySelectorAll(".search").forEach((row) => {
-        row.textContent.toLowerCase().includes(evt.target.value.toLowerCase()) //operador de sigo
-          ? row.classList.remove("filter") //no oculta
-          : row.classList.add("filter"); //oculta
-      });
+        // target referencia al objeto al cual se lanzo el evento
+        document.querySelectorAll(".search").forEach((row) => {
+            row.textContent.toLowerCase().includes(evt.target.value.toLowerCase()) //operador de sigo
+                ?
+                row.classList.remove("filter") //no oculta
+                :
+                row.classList.add("filter") //oculta
+
+
+        });
     }
-  });
+});
 
-   console.log(table);
-}
+document.getElementById("pantallados").style.display = "none";
+
+//pantalla uno
+
+document.getElementById('infoAtletas').addEventListener('click', function() {
+    document.getElementById("pantallaUno").style.display = "none";
+    document.getElementById("pantallados").style.display = ''; // show
+});
+
+//Inicio de pantalla dos
+document.getElementById('inicio').addEventListener('click', function() {
+    document.getElementById("pantallados").style.display = "none"; // show
+    document.getElementById("pantallaUno").style.display = '';
+});
+
 window.onload = filterByCGM("0");
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-  
-
-
-
